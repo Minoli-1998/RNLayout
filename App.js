d6f91d6e697a7733320446558450ca4a6eb37665
Minoli <mbhagyagunathilake@gmail.com>
2023-10-16 13:23:43 +0530

    changes added

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row-reverse",
+    justifyContent: "space-evenly",
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
