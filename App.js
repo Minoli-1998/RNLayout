93dd0f0e1d526de890fdc0cbc3472522fd1b0018
Minoli <mbhagyagunathilake@gmail.com>
2023-10-16 13:30:02 +0530

    changes added

--- App.js
+++ App.js
@@ -5,7 +5,9 @@ export default function App() {
   return (
     //<View style={{ backgroundColor: "plum", flex: 1 }}></View>
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#8e9b00", paddingVertical: 100 }}>
+        Box 1
+      </Box>
       <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
       {/*<Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
@@ -19,7 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row-reverse",
+    flexDirection: "row",
+    alignItems: "baseline",
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
